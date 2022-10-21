926e568c9ec4975741a5198da9e1d6c036fbdb64
Sébastien Benede <sebastien.benede@ratp.fr>
2022-10-21 15:01:02 +0200

    Add book controller

---
+++ src/main/java/com/example/demo/book/controller/BookController.java
@@ -0,0 +1,81 @@
+package com.example.demo.book.controller;
+
+import com.example.demo.book.dto.BookDto;
+import com.example.demo.book.entity.Book;
+import com.example.demo.book.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/library/v1")
+public class BookController {
+
+    @Autowired
+    private IBookService bs;
+
+    /**
+     * Create a book
+     *
+     * @param bookDto DTO corresponding to the book
+     */
+    @PostMapping
+    public void createBook(BookDto bookDto) {
+        Book book = new Book();
+        book.author = bookDto.author;
+        book.title = bookDto.title;
+        book.year = bookDto.year;
+
+        this.bs.save(book);
+    }
+
+    /**
+     * Get a book by his ID
+     *
+     * @param id book ID
+     * @return a book
+     */
+    @GetMapping(value = "/books/{id}")
+    public BookDto getBookById(@PathVariable Long id) {
+        Optional<Book> book = bs.findById(id);
+
+        if (book.isPresent()) {
+            BookDto dto = new BookDto();
+            dto.title = book.get().title;
+            dto.author = book.get().author;
+            dto.year = book.get().year;
+
+            return dto;
+        }
+
+        return null;
+    }
+
+    /**
+     * Get books by title or author's name. Books can be found by fulltext search.
+     *
+     * @param name title or author
+     * @return list of books corresponding to the searched text
+     */
+    @GetMapping(value = "/books")
+    public List<BookDto> getBooks(@PathVariable String name) {
+        List<Book> bookList = bs.findByTitleOrAuthor(name);
+        List<BookDto> books = new ArrayList<>();
+
+        for (int i = 0; i < bookList.size(); i++) {
+            Book book = bookList.get(i);
+
+            BookDto dto = new BookDto();
+            dto.title = book.title;
+            dto.author = book.author;
+            dto.year = book.year;
+
+            books.add(dto);
+        }
+
+        return books;
+    }
+}
